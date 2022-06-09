d0081f8d109d8d1dbbc377ab7c52750c988f4fce
tomas <tomas@TOMAS-PC>
2022-06-08 21:11:50 -0300

    Add project files.

---
+++ TestAPI/Controllers/MembersController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        public MembersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private static List<Member> members = new List<Member> { 
+            new Member { 
+                Id = 1,
+                Name = "Tomas",
+                LastName = "D'Aloisio",
+                BirthDate = new DateTime(1999, 03, 11)
+            },
+            new Member {
+                Id = 2,
+                Name = "Rocio",
+                LastName = "Albé",
+                BirthDate = new DateTime(2000, 06, 22)
+            }
+        };
+        private readonly DataContext _context;
+
+        [HttpGet("GetMembers")]
+        public async Task<ActionResult<List<Member>>> Get()
+        {
+            return Ok(await _context.Members.ToListAsync());
+        }
+
+        [HttpGet("GetMember/{id}")]
+        public async Task<ActionResult<Member>> Get(int id)
+        {
+            var member = members.FirstOrDefault(x => x.Id == id);
+            
+            return member != null ? Ok(member) : BadRequest("No se encontró el socio para el nro indicado.");
+        }
+
+        [HttpPost("AddMember")]
+        public async Task<ActionResult<List<Member>>> AddMember(Member member)
+        {
+            members.Add(member);
+            return Ok(members);        
+        }
+
+        [HttpPut("UpdateMember")]
+        public async Task<ActionResult<List<Member>>> UpdateMember(Member request)
+        {
+            var member = members.FirstOrDefault(x => x.Id == request.Id);
+
+            if (member == null)
+            {
+                BadRequest("No se encontró el socio.");
+            }
+
+            member.Update(request);
+
+            return Ok(members);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<Member>>> UpdateMember(int id)
+        {
+            var member = members.FirstOrDefault(x => x.Id == id);
+
+            if (member == null)
+            {
+                BadRequest("No se encontró el socio.");
+            }
+
+            members.Remove(member);
+
+            return Ok(members);
+        }
+
+
+    }
+}
---
+++ TestAPI/Models/Member.cs
@@ -0,0 +1,26 @@
+﻿namespace TestAPI.Models
+{
+    public class Member
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string Barcode { get; set; } = String.Empty;
+
+        public DateTime BirthDate { get; set; }
+
+
+        public void Update(Member member)
+        {
+            Name = member.Name;
+            LastName = member.LastName; 
+            Barcode = member.Barcode;
+            BirthDate = member.BirthDate;
+        }
+    
+    }
+}
